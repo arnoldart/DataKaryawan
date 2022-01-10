eb8c61dba8a786b75525bc1c626c0e180c54c030
arnoldart <adoni.091203@gmail.com>
2022-01-10 09:59:46 +0700

    feat: implements CRUD

--- src/database2/connection.java
+++ src/database2/connection.java
@@ -10,7 +10,34 @@ public class connection {
             String password = "";
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/datakaryawan2", username, password);
             Statement stmt = connection.createStatement();
-            stmt.executeUpdate("INSERT INTO `karyawan` (`id_karyawan`, `nama_karyawan`, `tempat_lahir`, `tgl_lahir`, `alamat`, `pendidikan`, `status`) VALUES (NULL, 'Jedidta', 'Jogjakarta', '2003-09-12', 'Gamping', 'S1', 'tetap')");
+
+            /*UPDATE*/
+//            stmt.executeUpdate("UPDATE karyawan SET nama_karyawan='Jedidta Adoni Saputra'");
+            
+            /*SELECT*/
+//            ResultSet result = stmt.executeQuery("SELECT * FROM karyawan");
+//            while (result.next()) {
+//                String Nama = result.getString("nama_karyawan");
+//                String TempatLahir = result.getString("tempat_lahir");
+//                String tglLahir = result.getString("tgl_Lahir");
+//                String Alamat = result.getString("alamat");
+//                String Pendidikan = result.getString("pendidikan");
+//                String Status = result.getString("status");
+//
+//                System.out.println("Nama Karyawan " + Nama);
+//                System.out.println("Tempat Lahir " + TempatLahir);
+//                System.out.println("Tanggal Lahir " + tglLahir);
+//                System.out.println("Alamat " + Alamat);
+//                System.out.println("Pendidikan " + Pendidikan);
+//                System.out.println("Status " + Status);
+//            }
+            
+            /*INSERT*/
+            // stmt.executeUpdate("INSERT INTO `karyawan` (`id_karyawan`, `nama_karyawan`, `tempat_lahir`, `tgl_lahir`, `alamat`, `pendidikan`, `status`) VALUES (NULL, 'Jedidta', 'Jogjakarta', '2003-09-12', 'Gamping', 'S1', 'tetap')");
+
+            /*DELETE*/
+//            stmt.executeUpdate("DELETE FROM karyawan WHERE id_karyawan=3");
+
         } catch ( SQLException e ) {
             System.out.println("koneksi gagal");
         }
